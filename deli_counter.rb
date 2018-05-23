0a63350ed2e270aea8b96e9a09d743f1be617ea7
Isiah VanBrackle <squarepheonix@gmail.com>
2018-05-23 17:17:37 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -12,7 +12,7 @@ end
 
 def take_a_number(array, name)
   array<< name
-  puts  "Welcome, you are number #{array.index(name) + 1}."
+  puts  "Welcome #{name}, you are number #{array.index(name) + 1}."
 end
 
 def now_serving(array)
